3ecac0ce31cc29c086ecd61fe71a9b78e644bdec
hyndavi217 <hyndavi217@gmail.com>
2020-04-13 16:24:51 -0400

    added  ConversationHandler Controller

--- src/main/java/com/umd/webservicesProject/project2/Project2Application.java
+++ src/main/java/com/umd/webservicesProject/project2/Project2Application.java
@@ -2,8 +2,10 @@ package com.umd.webservicesProject.project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value="com.umd.webservicesProject.project2.controller")
 public class Project2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/umd/webservicesProject/project2/controller/ConversationHandlerController.java
@@ -0,0 +1,15 @@
+package com.umd.webservicesProject.project2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConversationHandlerController {
+	
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hi dear!";
+	}
+
+}
